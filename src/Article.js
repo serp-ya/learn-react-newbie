3c980cad1b3e0a2dda7c032ace7d22b3ade146a3
serp-ya <sergei.kundryukov1989@gmail.com>
2018-02-15 23:19:38 +0300

    Добавлен вывод комментариев на странице

---
+++ src/Article.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import CommentsList from './CommentsList'
+
+export default class Article extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: true
+        };
+    }
+
+    render() {
+        const {article} = this.props;
+        const {isOpen} = this.state;
+        return (
+            <div>
+                <h3>{article.title}</h3>
+                <button onClick = {this.toggleOpen}>
+                    {isOpen ? 'close' : 'open'}
+                </button>
+                {this.getBody()}
+            </div>
+        );
+    }
+
+    getBody() {
+        if (!this.state.isOpen) return null;
+        const {article} = this.props;
+        const {comments} = article;
+        return (
+          <section>
+            {article.text}
+            <CommentsList comments={comments || []}/>
+          </section>
+        );
+    }
+
+    toggleOpen = (ev) => {
+        ev.preventDefault();
+        console.log('---', ev.nativeEvent);
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+}
