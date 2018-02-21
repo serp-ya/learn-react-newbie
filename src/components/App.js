d86471559e56dd94a64a1103879b2eb8d0cc9d94
serp-ya <sergei.kundryukov1989@gmail.com>
2018-02-22 00:30:06 +0300

    Добавил стили селекту

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ArticleList from './ArticleList';
 import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 export default class App extends Component {
-  static propTypes = {
+  // static propTypes = {
+  //
+  // };
 
+  state = {
+    selection: null
   };
 
   render() {
+    const { selection } = this.state;
+    const options = this.props.articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }));
+
     return (
       <div>
+        <Select options = {options} value = {selection} onChange = {this.changeSelection} />
         <ArticleList articles = { this.props.articles }/>
       </div>
-    )
+    );
   }
+
+  changeSelection = (selection) => this.setState({ selection });
 }
