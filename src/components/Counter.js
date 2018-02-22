9102618dd0b95f0079261a5bb99dbc29a5286954
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-02-22 18:26:34 +0300

    Компонент Counter настроен на редакс

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { increment } from "../ActionCreators";
 
-export default class Counter extends Component {
+class Counter extends Component {
     static propTypes = {
-
+        counter: PropTypes.number,
+        increment: PropTypes.func.isRequired
     };
 
     render() {
         return (
             <div>
-
+                <h2>{this.props.counter}</h2>
+                <button onClick = {this.handleIncrement}>+1</button>
             </div>
         )
     };
+
+    handleIncrement = () => {
+        this.propsincrement();
+    }
 }
+
+const mapStateToProps = (state) => (
+    { counter: state.count }
+);
+
+const mapToDispatch = { increment };
+const decorator = connect(mapStateToProps, mapToDispatch);
+
+export default decorator(Counter);
