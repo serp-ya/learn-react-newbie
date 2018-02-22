d71c2c30fc0fa1d3060aa87c5de08400137740d8
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-02-22 17:20:29 +0300

    Добавлен стор из редакс

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/App';
 import {articles} from './fixtures';
+import store from './store';
 
 render(<App articles = {articles} />, document.getElementById('container'));
